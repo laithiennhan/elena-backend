213815e8a763db789e45e2131aadf9f369f5aeb5
laithiennhan <ntlai@umass.edu>
2023-05-08 17:16:40 -0400

    complete dijkstra algorithm

--- algorithm.py
+++ algorithm.py
@@ -3,62 +3,59 @@ import networkx as nx
 import heapq
 
 def elevation_gain(graph, start, end):
-	""" Calculate the elevation gain between two nodes
+    """ Calculate the elevation gain between two nodes
 
-	Args:
-		graph (NetworkX graph): Graph that contains 2 nodes
-		start (_type_): _description_
-		end (_type_): _description_
+    Args:
+        graph (NetworkX graph): Graph that contains 2 nodes
+        start (_type_): _description_
+        end (_type_): _description_
 
-	Returns:
-		_type_: _description_
-	"""
-	ele = graph.nodes[end]['elevation'] - graph.nodes[start]['elevation']
-	if ele > 0: 
-		return ele
-	else: 
-		return 0
+    Returns:
+        _type_: _description_
+    """
+    ele = graph.nodes[end]['elevation'] - graph.nodes[start]['elevation']
+    return max(0, ele)
 
 def distance(graph, node1, node2):
-	"""Distance between two nodes
+    """Distance between two nodes
 
-	Args:
-		graph (_type_): _description_
-		node1 (_type_): _description_
-		node2 (_type_): _description_
+    Args:
+        graph (_type_): _description_
+        node1 (_type_): _description_
+        node2 (_type_): _description_
 
-	Returns:
-		_type_: _description_
-	"""
-	return graph.edges[node1, node2, 0]['length']
+    Returns:
+        _type_: _description_
+    """
+    return graph.edges[node1, node2, 0]['length']
 
 def path_length(graph, path):
-	"""Return the total distance of a path
+    """Return the total distance of a path
 
-	Args:
-		graph (_type_): _description_
-		path (_type_): _description_
-	"""    
-	length = 0
-	for i in range(len(path) - 1):
-		length += distance(graph, path[i], path[i+1])
-	return length
+    Args:
+        graph (_type_): _description_
+        path (_type_): _description_
+    """    
+    length = 0
+    for i in range(len(path) - 1):
+        length += distance(graph, path[i], path[i+1])
+    return length
 
 def path_elevation(graph, path):
-	"""Calculate total elevation gain in path
+    """Calculate total elevation gain in path
 
-	Args:
-		graph (_type_): _description_
-		path (_type_): _description_
+    Args:
+        graph (_type_): _description_
+        path (_type_): _description_
 
-	Returns:
-		_type_: _description_
-	"""    
-	total_elevation = 0
-	for i in range(len(path) - 1):
-		total_elevation += elevation_gain(graph, path[i], path[i+1])
-	
-	return total_elevation
+    Returns:
+        _type_: _description_
+    """    
+    total_elevation = 0
+    for i in range(len(path) - 1):
+        total_elevation += elevation_gain(graph, path[i], path[i+1])
+    
+    return total_elevation
 
 def nearest_node(g, x, y):
     # Find the nearest node to the given coordinates
@@ -80,73 +77,88 @@ def path_coordinates(G, path):
     return coord_list
 
 def dijkstra_path(graph, start, end, percent, maximize):
-	"""Dijkstra algorithm 
+    """Dijkstra algorithm 
 
-	Args:
-		graph (_type_): _description_
-		start (_type_): _description_
-		end (_type_): _description_
-		percent (_type_): _description_
-		maximize (_type_): _description_
-	"""
+    Args:
+        graph (_type_): _description_
+        start (_type_): _description_
+        end (_type_): _description_
+        percent (_type_): _description_
+        maximize (_type_): _description_
+    """
+        
+    shortest_path = ox.shortest_path(graph, start, end)
+    
+    shortest_path_length = path_length(graph, shortest_path)
  
-	shortest_path = ox.shortest_path(graph, start, end)
-	
-	shortest_path_length = path_length(graph, shortest_path)
+    distance_constraint = shortest_path_length * percent / 100
  
-	distance_constraint = shortest_path_length * percent / 100
- 
-	# Elevation cost function
-	def elevation_cost(node1, node2):
-		node1_elevation = graph.nodes[node1]['elevation']
-		node2_elevation = graph.nodes[node2]['elevation']
-		if maximize:
-			return max(0, node2_elevation - node1_elevation)
-		else:
-			return max(0, node1_elevation - node2_elevation)
+    # Elevation cost function
+    def elevation_cost(node1, node2):
+        node1_elevation = graph.nodes[node1]['elevation']
+        node2_elevation = graph.nodes[node2]['elevation']
+        # if maximize:
+        # 	return min(0, node1_elevation - node2_elevation)
+        # else:
+        return max(0, node2_elevation - node1_elevation)
 
-	# Dijkstra algorithm for route with respect to elevation cost
-	distances = {node: float('inf') for node in graph.nodes}
-	predecessors = {node: None for node in graph.nodes}	
+    # Dijkstra algorithm for route with respect to elevation cost
+    distances = {node: float('inf') for node in graph.nodes}
+    predecessors = {node: None for node in graph.nodes}	
+    elevations = {node: float('inf') for node in graph.nodes}
+    
     # Priority queue:
-	queue = []
-	path = []
-	distances[start] = 0
-	heapq.heappush(queue, (elevation_cost(start, end), start))
-	
-	while queue:
+    p_queue = []
+    path = []
+    elevations[start] = 0
+    distances[start] = 0
+    if maximize:
+        heapq.heappush(p_queue, (-elevation_cost(start, end), start))
+    else:
+        heapq.heappush(p_queue, (elevation_cost(start, end), start))
+    while p_queue:
         # Get the node with the lowest estimated cost to the goal
-		current_cost, current_node = heapq.heappop(queue)
+        current_cost, current_node = heapq.heappop(p_queue)
 
         # If we reached the goal, reconstruct the path and return it
-		if current_node == end:
-			path = []
-			while current_node:
-				path.append(current_node)
-				current_node = predecessors[current_node]
-			return path[::-1]
+        if current_node == end:
+            print("here")
+            path = []
+            while current_node:
+                path.append(current_node)
+                current_node = predecessors[current_node]
+            return path[::-1]
 
         # Check the neighboring nodes
-		for curr, neighbor, data in graph.edges(current_node, data=True):
+        for curr, neighbor, data in graph.edges(current_node, data=True):
             # Calculate the elevation gain between the current node and the neighbor
-
+            curr_elevation = elevation_cost(current_node, neighbor)
+            curr_distance = distance(graph, current_node, neighbor)
             # Calculate the total distance of the path so far
-			total_distance = distances[current_node] + distance(graph, curr, neighbor)
+            total_elevation = elevations[current_node] + curr_elevation
+            total_distance = distances[current_node] + curr_distance
 
             # Check if the total distance exceeds x% of the shortest path
-			if total_distance > distance_constraint:
-				continue
+            if total_distance > distance_constraint:
+                continue
 
             # Update the distance and predecessor if the new distance is shorter
-			if total_distance < distances[neighbor]:
-				distances[neighbor] = total_distance
-				predecessors[neighbor] = current_node
+            if total_elevation < elevations[neighbor]:
+                elevations[neighbor] = total_elevation
+                distances[neighbor] = total_distance
+                predecessors[neighbor] = current_node
 
                 # Add the neighbor to the priority queue with its estimated cost to the goal
-				heapq.heappush(queue, (elevation_cost(neighbor, end), neighbor))
+                if maximize:
+                    heapq.heappush(p_queue, (-total_elevation, neighbor))
+                else:
+                    heapq.heappush(p_queue, (total_elevation, neighbor))
+                
 
-    # If we didn't find a path, return an empty list
-	return []
+    # If we didn't find a path, return the shortest path
+    return shortest_path
+
+
 
     
     
